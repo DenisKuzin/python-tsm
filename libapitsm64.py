1fef09e5a962f64ad1dd84d25ae24b05927bacbc
Denis Kuzin <digiholiq@yandex.ru>
2014-11-25 15:26:30 +0300

    API Version

--- libapitsm64.py
+++ libapitsm64.py
@@ -1,6 +1,33 @@
-from ctypes import cdll
+from ctypes import cdll, c_char, c_ubyte, c_short, c_ushort, c_int, c_uint, c_ulong, c_long, byref, Structure
 
 LIBRARY_FILENAME = 'libApiTSM64.so'
+api_lib = cdll.LoadLibrary(LIBRARY_FILENAME)
+
+dsInt8_t = c_char
+dsUint8_t = c_ubyte
+dsInt16_t = c_short
+dsUint16_t = c_ushort
+dsInt32_t = c_int
+dsUint32_t = c_uint
+dsULong_t = c_ulong
+dsLong_t = c_long
+dsInt64_t = c_long
+dsUint64_t = c_long
+
+class dsmApiVersionEx(Structure):
+    _fields_ = [
+        ('stVersion', dsUint16_t),
+        ('stVersion', dsUint16_t),
+        ('version', dsUint16_t),
+        ('release', dsUint16_t),
+        ('level', dsUint16_t),
+        ('subLevel', dsUint16_t),
+    ]
+
+def dsmQueryApiVersionEx():
+    ver = dsmApiVersionEx()
+    api_lib.dsmQueryApiVersionEx(byref(ver))
+    return ver
 
 if __name__ == '__main__':
-    api_lib = cdll.LoadLibrary(LIBRARY_FILENAME)
+    ver = dsmQueryApiVersionEx()
--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@
 
 TSM_INSTALL_DIR='/opt/tivoli'
 
+export DSMI_DIR=$TSM_INSTALL_DIR/tsm/client/ba/bin
+
+export DSMI_CONFIG=$DSMI_DIR/dsm.opt
+
+export DSMI_LOG=/var/log/
+
 export LD_LIBRARY_PATH=$TSM_INSTALL_DIR/tsm/client/api/bin64:$LD_LIBRARY_PATH
 
 python libapitsm64.py
